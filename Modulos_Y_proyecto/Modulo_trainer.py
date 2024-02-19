73183e8391982c68562e6245e41c35b75e683d96
darman-prog <149440307+darman-prog@users.noreply.github.com>
2024-02-19 11:58:13 -0500

    final Update Modulo_trainer.py

--- Modulos_Y_proyecto/Modulo_trainer.py
+++ Modulos_Y_proyecto/Modulo_trainer.py
@@ -1,43 +1,71 @@
-from utils import guardar_datos_trainer
-
-
-
-
+from utils import guardar_datos_trainer #aca estan los datos guardados de trainer
+#registro en el sistema de trainer nuevo
 def registro_trainer():
     datos_trainer = []
-    print("Bienvenido a Registro de Trainer")
-    print(" Ingresa Tus Datos Para Registrarte En el sistema y Asignarte una Clase ")
-    while True:
-       try: 
-         nombre = input(" Ingresa tu nombre Completo ")
-         break
-       except ValueError:
-           print("Ingresa Nombre Valido")  
-        
-    datos_trainer.append(nombre)
+    print("Bienvenido al Registro de Trainer")
+    print("Ingresa tus datos para registrarte en el sistema ")
     
     while True:
-       try:
-          numero_de_trainer = int(input("Ingresa tu Numero Telefonico"))
-          break       
-       except ValueError:
-           print ("Ingresa Numero valido")
-    datos_trainer.append(numero_de_trainer)
-    
-    while True:
-       try:
-           documento_trainer=int(input("Ingresa Tu numero de Documento"))
-           break       
-       except ValueError:
-           print ("Ingresa Numero valido")
-    datos_trainer.append(documento_trainer)
-    while True:
-       try:
-            Conocimientos_trainer = input("Que Conocimientos Tienes En Programacion ")
+        nombre = input("Ingresa tu nombre completo: ")
+        if nombre:
+            datos_trainer.append(nombre)
             break
-       except ValueError:
-            print("Ingresa Otra vez")       
-    datos_trainer.append(Conocimientos_trainer)
-    ruta_archivo = "datos_trainer.json"  
+        else:
+            print("El nombre no puede estar vacío. Por favor, ingrésalo nuevamente.")
+        
+    while True:
+        try:
+            numero_de_trainer = int(input("Ingresa tu número telefónico: "))
+            datos_trainer.append(numero_de_trainer)
+            break
+        except ValueError:
+            print("Ingresa un número de teléfono válido.")
     
-    guardar_datos_trainer(datos_trainer, ruta_archivo)
+    while True:
+        try:
+            documento_trainer = int(input("Ingresa tu número de documento: "))
+            datos_trainer.append(documento_trainer)
+            break
+        except ValueError:
+            print("Ingresa un número de documento válido.")
+    
+    conocimientos_trainer = input("Conocimientos de Programacion ")
+    datos_trainer.append(conocimientos_trainer)
+
+    while True:
+        salon_asignado = input("Ingresa el salón asignado: ")
+        if salon_asignado:
+            datos_trainer.append(salon_asignado)
+            break
+        else:
+            print("El salón no puede estar vacío. Por favor, ingrésalo nuevamente.")
+
+    while True:
+        ruta_asignada = input(" Ingresa la ruta asignada: ")
+        if ruta_asignada:
+            datos_trainer.append(ruta_asignada)
+            break
+        else:
+            print("La ruta no puede estar vacía. Por favor, ingrésala nuevamente.")
+    
+    print(" Trainer Agregado Exitosamente!! ")
+    guardar_datos_trainer(datos_trainer)
+
+#ver horarios para el trainer
+def ver_horarios():
+     opcion_horario = input("Escoje diurno o nocturno").strip().lower()
+     horario_Diurno = [
+         {"artemis": "6 AM Hasta 2 PM"},
+         {"Sputnik": "6 AM Hasta 2 PM"},
+         {"Apolo": "6 AM Hasta 2 PM"}
+     ]
+     horario_Nocturno = [
+         {"Artemis": "2 PM Hasta 10 PM"},
+         {"Sputnik": "2 PM Hasta 10 PM"},
+         {"Apolo": "2 PM Hasta 10 P M"}
+     ]
+
+     if opcion_horario == "diurno":
+         print(horario_Diurno)
+     elif opcion_horario == "nocturno":
+        print(horario_Nocturno)
