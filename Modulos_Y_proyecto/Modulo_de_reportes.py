260d4a5bfc378077bf0e0464cb45c01d92e3bc0f
darman-prog <149440307+darman-prog@users.noreply.github.com>
2024-02-19 11:57:32 -0500

    final Update Modulo_de_reportes.py

--- Modulos_Y_proyecto/Modulo_de_reportes.py
+++ Modulos_Y_proyecto/Modulo_de_reportes.py
@@ -1,34 +1,53 @@
-import os
 import json
-from utils import generar_documentos
-from utils import cargar_documentos
-from utils import guardar_documentos
-from utils import ordenar_documentos_por_nombre
-
-
-def campers_incristos_lista():
-    
-    if os.path.exists("Campers_Documentacion.json"):
-        listado_de_documentos = cargar_documentos()
-    else:
-        listado_de_documentos = generar_documentos()
-        guardar_documentos(listado_de_documentos)
-        campers_inscritos_ordenados = ordenar_documentos_por_nombre(listado_de_documentos)
-        for camper in campers_inscritos_ordenados:
-          print(camper)
-
-def listar_trainers():
+#ver campers general
+def ver_documentos():
     try:
-        with open("datos_trainers.json","r") as file :
-            trainers = json.load(file)
-
-            for trainer in trainers:
-                 print("Nombre:", trainer["nombre"])
-                 print("Teléfono:", trainer["telefono"])
-                 print("Documento:", trainer["documento"])
-                 print("Conocimientos:", trainer["conocimientos"])
-                 print()  
+        with open("campers_Documentacion.json", "r") as file:
+            documentos = json.load(file)
+            for documento in documentos:
+                print(documento)
     except FileNotFoundError:
-        print("Archivo json No existe")
+        print("No se encontraron documentos.")
 
 
+#reportes de campers en riesgo y estados generales
+def listar_camper_por_estado(estado):
+    try:
+        with open("campers_Documentacion.json", "r") as file:
+            documentos = json.load(file)
+            campers_filtrados = [camper for camper in documentos if "Estado Del camper" in camper and camper["Estado Del camper"] == estado]
+            for camper in campers_filtrados:
+                print(camper)
+    except FileNotFoundError:
+        print("No se encontraron documentos.")
+
+
+def listar_camper_por_riesgo(riesgo):
+    try:
+        with open("campers_Documentacion.json", "r") as file:
+            documentos = json.load(file)
+            campers_filtrados = [camper for camper in documentos if "Riesgo Del Camper" in camper and camper["Riesgo Del Camper"] == riesgo]
+            for camper in campers_filtrados:
+                print(camper)
+    except FileNotFoundError:
+        print("No se encontraron documentos.")
+#fin 
+
+def imprimir_trainers():
+    try:
+        with open("trainers.json", "r") as file:
+            trainers = json.load(file)
+            if trainers:
+                print("Datos de los Trainers:")
+                for index, trainer in enumerate(trainers, 1):
+                    print(f"Trainer {index}:")
+                    print(f"Nombre: {trainer['Nombre']}")
+                    print(f"Documento: {trainer['Documento']}")
+                    print(f"Número de Teléfono: {trainer['Numero Telefono']}")
+                    print(f"Ruta: {trainer['Ruta']}")  # Muestra la ruta seleccionada para el entrenador
+                    print(f"Salón Asignado: {trainer['Salon Asignado']}")
+                    print()
+            else:
+                print("No se encontraron trainers en el archivo JSON.")
+    except FileNotFoundError:
+        print("El archivo 'trainers.json' no existe.")
